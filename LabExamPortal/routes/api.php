1e965780ef97518fdd3d384bd01acf1d2fcc93f6
HEMRAJ KUMAWAT <61903212+hemraj007@users.noreply.github.com>
2020-07-11 19:43:04 +0530

    Added new Route to update duration

--- LabExamPortal/routes/api.php
+++ LabExamPortal/routes/api.php
@@ -32,6 +32,7 @@ Route::get('fetch_course','API\UserController@fetch_course');
 Route::get('startExam','API\ExamController@startExam');
 Route::get('fetch_source_code','API\ExamController@fetch_source_code');
 Route::post('save_source','API\ExamController@save_source');
+Route::put('update_duration','API\ExamController@update_duration');
 Route::post('logout','API\UserController@logout');
 Route::get('details', 'API\UserController@details');
 Route::get('number_of_online_users', 'API\AdminController@number_of_online_users');
