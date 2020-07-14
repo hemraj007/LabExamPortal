8920c0041392daac6cf32636962ec4829d37f014
HEMRAJ KUMAWAT <hemraj.official007@gmail.com>
2020-07-14 21:41:49 +0530

    fillable in exam_detail model

--- LabExamPortal/app/exam_detail.php
+++ LabExamPortal/app/exam_detail.php
@@ -8,4 +8,8 @@ class exam_detail extends Model
 {
     //
     protected $table = 'exam_details';
+
+    protected $fillable = [
+        'exam_id','exam_name','exam_hours','exam_for','exam_code','exam_date','exam_time',
+    ];
 }
